c78996a041fc2b339014979240aa42bd6093bcb8
aryansam <samanta2@purdue.edu>
2025-03-06 11:38:56 -0600

    date fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -166,6 +166,7 @@ for (let i = 0; i < navigationLinks.length; i++) {
     for (let i = 0; i < pages.length; i++) {
       if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
         pages[i].classList.add("active");
+        console.log(navigationLinks[i].classList);
         navigationLinks[i].classList.add("active");
         window.scrollTo(0, 0);
       } else {
